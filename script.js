a4d702570ad775e7be14901ad8893816d8869632
Quinterro <93486127+Quinterro@users.noreply.github.com>
2022-10-10 14:08:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-let distanceMax = 1000000;
-let distanceMin = 0;
-let distanceDefault = 0;
+const distanceMax = 1000000;
+const distanceMin = 0;
+const distanceDefault = 0;
 
-let yearDriversLicense = document.getElementById("yearDriversLicense").value;
+const yearDriversLicense = document.getElementById("yearDriversLicense").value;
 
-let today = dayjs();
-let tomorrow = dayjs().add(1, "day");
+const today = dayjs();
+const tomorrow = dayjs().add(1, "day");
 
-let oilPrice = 7.6;
-let priceCategory = "Medium";
+const priceCategories = {
+  basic: {
+    name: "Basic",
+    factor: 1,
+  },
+  standard: {
+    name: "Standard",
+    factor: 1.3,
+  },
+  medium: {
+    name: "Medium",
+    factor: 1.6,
+  },
+  premium: {
+    name: "Premium",
+    factor: 2,
+  },
+};
+
+const oilPrice = 7.6;
+const priceCategory = priceCategories.medium.name;
 let localization = "Rzeszow";
-let bazowaloanPrice = 100;
-let mileage = 10;
-let availableModel = 2;
+const baseloanPrice = 100;
+const mileage = 10;
+const availableModel = 2;
 
 let car = {
   priceCategory: priceCategory,
@@ -21,33 +40,30 @@ let car = {
   localization: localization,
 };
 
-let hours = 24;
-let minutes = 60;
-let seconds = 60;
-let miliseconds = 1000;
+const hours = 24;
+const minutes = 60;
+const seconds = 60;
+const miliseconds = 1000;
 
-let factorBasic = 1;
-let factorStandard = 1.3;
-let factorMedium = 1.6;
-let factorPremium = 2;
+const currentYear = today.year();
 
-let currentYear = today.year();
+const driversLongevityRate = 1.2;
+const amountOfCarsRate = 1.15;
+const vatRate = 1.23;
 
-let driversLongevityRate = 1.2;
-let amountOfCarsRate = 1.15;
-let vatRate = 1.23;
+const distanceContainer = document.getElementById("distance");
+distanceContainer.setAttribute("max", distanceMax);
+distanceContainer.setAttribute("min", distanceMin);
+distanceContainer.setAttribute("value", distanceDefault);
 
-document.getElementById("distance").setAttribute("max", distanceMax);
-document.getElementById("distance").setAttribute("min", distanceMin);
-document.getElementById("distance").setAttribute("value", distanceDefault);
-
-document
-  .getElementById("yearDriversLicense")
-  .setAttribute("max", tomorrow.year);
-document.getElementById("yearDriversLicense").setAttribute("min", 1918);
-document
-  .getElementById("yearDriversLicense")
-  .setAttribute("value", tomorrow.subtract(2, "year").year());
+const yearDriversLicenseContainer =
+  document.getElementById("yearDriversLicense");
+yearDriversLicenseContainer.setAttribute("max", tomorrow.year);
+yearDriversLicenseContainer.setAttribute("min", 1918);
+yearDriversLicenseContainer.setAttribute(
+  "value",
+  tomorrow.subtract(2, "year").year()
+);
 
 document
   .getElementById("start")
@@ -65,17 +81,16 @@ document
   .getElementById("stop")
   .setAttribute("max", today.add(100, "day").format("YYYY-MM-DD"));
 
-let startLoan = document.getElementById("start").value;
-let dateStart = dayjs(startLoan);
-//let dateStart = new Date(startLoan).getTime();
-let stopLoan = document.getElementById("stop").value;
-let dateStop = dayjs(stopLoan);
-//let dateStop = new Date(stopLoan).getTime();
-let priceDaily = dateStop.diff(dateStart, "day"); // (miliseconds * seconds * minutes * hours);
+const startLoan = document.getElementById("start").value;
+const dateStart = dayjs(startLoan);
+
+const stopLoan = document.getElementById("stop").value;
+const dateStop = dayjs(stopLoan);
+
+let priceDaily = dateStop.diff(dateStart, "day");
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  //----- console.log(e);
 
   let loanPrice = 100 * priceDaily;
   let wholeLoanPrice = 0;
@@ -83,32 +98,28 @@ document.getElementById("form").addEventListener("submit", (e) => {
 
   switch (car.priceCategory) {
     case "Basic":
-      wholeLoanPrice *= factorBasic;
+      wholeLoanPrice *= priceCategories.basic.factor;
       break;
     case "Standard":
-      wholeLoanPrice *= factorStandard;
+      wholeLoanPrice *= priceCategories.medium.factor;
       break;
     case "Medium":
-      wholeLoanPrice *= factorMedium;
+      wholeLoanPrice *= priceCategories.medium.factor;
       break;
     case "Premium":
-      wholeLoanPrice *= factorPremium;
+      wholeLoanPrice *= priceCategories.premium.factor;
       break;
   }
 
-  if (currentYear - yearDriversLicense < 5) {
+  if (currentYear - yearDriversLicense < 5)
     wholeLoanPrice *= driversLongevityRate;
-  }
 
-  if (currentYear - yearDriversLicense < 3 && car.priceCategory == "Premium") {
+  if (currentYear - yearDriversLicense < 3 && car.priceCategory == "Premium")
     window.prompt(
       "Nie możesz wypożyczyć tego samochodu, ponieważ aby wypożyczyć samochód klasy Premium, potrzebujesz posiadać prawo jazdy conajmniej 3 lata."
     );
-  }
 
-  if (availableModel < 3) {
-    wholeLoanPrice *= amountOfCarsRate;
-  }
+  if (availableModel < 3) wholeLoanPrice *= amountOfCarsRate;
 
   let oilCost =
     (document.getElementById("distance").value / 100) * car.mileage * oilPrice;
@@ -119,23 +130,21 @@ document.getElementById("form").addEventListener("submit", (e) => {
 
   document.getElementById(
     "oilPrice"
-  ).innerHTML = `<br><br>Szacunkowa cena za paliwo: ${oilCost.toFixed(
-    2
-  )} zł<br>`;
+  ).innerHTML = `Szacunkowa cena za paliwo: ${oilCost.toFixed(2)} zł`;
 
   document.getElementById(
     "onlyLoan"
-  ).innerHTML = `Cena za samo wypożyczenie: ${loanPrice.toFixed(2)} zł<br>`;
+  ).innerHTML = `Cena za samo wypożyczenie: ${loanPrice.toFixed(2)} zł`;
 
   document.getElementById(
     "priceNetto"
   ).innerHTML = `Cena netto za całe wypożyczenie: ${wholeLoanPrice.toFixed(
     2
-  )} zł<br>`;
+  )} zł`;
 
   document.getElementById(
     "priceBrutto"
   ).innerHTML = `Cena brutto za całe wypożyczenie: ${priceBrutto.toFixed(
     2
-  )} zł<br>`;
+  )} zł`;
 });
